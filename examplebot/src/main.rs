49cd567428cdeb1f7436e3cc815858de63030a69
cerebellum <cere@fastmail.fm>
2019-10-23 01:12:14 +0200

    Simplify config builder, remove dead code

--- examplebot/src/main.rs
+++ examplebot/src/main.rs
@@ -5,20 +5,18 @@ extern crate log;
 
 use std::env;
 use std::error::Error;
-use tmi_rs::client::TwitchClient;
-use tmi_rs::ClientConfigBuilder;
+use tmi_rs::{TwitchClientBuilder, TwitchClient};
 use tmi_rs::futures::StreamExt;
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
-    let config = ClientConfigBuilder::default()
+    let client: TwitchClient = TwitchClientBuilder::default()
         .username(env::var("TWITCH_USERNAME")?)
         .token(env::var("TWITCH_AUTH")?)
         .cap_membership(true)
         .build()?;
-    let client = TwitchClient::new(config);
     let mut connection = client.connect().await?;
 
     connection.join("forsen").await?;
